b7ed3e6da928a8847925d551aed873f1149d451e
powerworker1234@gmail.com <powerworker1234@gmail.com>
2024-02-29 11:11:17 +0800

    initial commit

---
+++ app/src/main/java/com/example/practice0229/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.practice0229
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        R.id.Hello;
+        R.drawable.pig;
+        R.color.message;
+        R.string.message_name;
+
+    }
+}
