ae1efa485e0cc3122540062107700aecca767a60
powerworker1234@gmail.com <powerworker1234@gmail.com>
2024-03-05 18:55:29 +0800

    0305

--- app/src/main/java/com/example/practice0229/MainActivity.kt
+++ app/src/main/java/com/example/practice0229/MainActivity.kt
@@ -2,15 +2,22 @@ package com.example.practice0229
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+
+//import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    val secretNumber=SecretNumber()
+    lateinit var number:EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        R.id.Hello;
-        R.drawable.pig;
-        R.color.message;
-        R.string.message_name;
-
+        number=findViewById<EditText>(R.id.number)
+    }
+    fun check(view:View){
+        val n = number.text.toString().toInt()
+        println("number: $n")
     }
 }
